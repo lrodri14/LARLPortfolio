69ae6cdca4f5f8ecd55084b87d05997ad0ff267f
Luis Adolfo Rodriguez Lopez <adolfoluis.rl@gmail.com>
2021-10-28 18:06:31 -0600

    Create UI Navbar component with routing setup

--- lpf/src/App.js
+++ lpf/src/App.js
@@ -1,23 +1,27 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import { Route, Switch } from 'react-router-dom'
 import './App.css';
 import Main from './pages/Main'
 import Skills from './pages/Skills'
 import Projects from './pages/Projects'
+import Navbar from './components/UI/Navbar'
 
 function App() {
     return (
-        <Switch>
-            <Route path="/" exact>
-                <Main/>
-            </Route>
-            <Route path="/skills">
-                <Skills/>
-            </Route>
-            <Route path="/projects">
-                <Projects/>
-            </Route>
-        </Switch>
+        <Fragment>
+            <Navbar/>
+            <Switch>
+                <Route path="/" exact>
+                    <Main/>
+                </Route>
+                <Route path="/skills">
+                    <Skills/>
+                </Route>
+                <Route path="/projects">
+                    <Projects/>
+                </Route>
+            </Switch>
+        </Fragment>
     )
 }
 
