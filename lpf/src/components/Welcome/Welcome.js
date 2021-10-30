641d1e99006abb590e5e005692b0c9d9aa8453ca
Luis Adolfo Rodriguez Lopez <adolfoluis.rl@gmail.com>
2021-10-29 21:19:54 -0600

    Update linkedin anchor target

--- lpf/src/components/Welcome/Welcome.js
+++ lpf/src/components/Welcome/Welcome.js
@@ -10,7 +10,7 @@ function Welcome(){
             <Link to='/about-me' className={styles['welcome__about-me-button']}><i className="fas fa-play"></i> About Me</Link>
         </div>
         <img className={styles['profile-pic']} src='/pp-bw.png' alt="Luis Rodriguez's Portfolio Pic" />
-        <a href='https://www.linkedin.com' target="_blank"  rel="noreferrer" className={styles.linkedIn}><i className="fab fa-linkedin"></i></a>
+        <a href='https://www.linkedin.com/in/larl/' target="_blank"  rel="noreferrer" className={styles.linkedIn}><i className="fab fa-linkedin"></i></a>
     </ Fragment>)
 }
 
