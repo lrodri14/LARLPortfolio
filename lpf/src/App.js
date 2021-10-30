d75fdc18acf9125b6e3ec929776459641747d964
Luis Adolfo Rodriguez Lopez <adolfoluis.rl@gmail.com>
2021-10-30 08:37:36 -0600

    Create Contact Me Page, and update routing setup

--- lpf/src/App.js
+++ lpf/src/App.js
@@ -1,20 +1,25 @@
 import React, { Fragment } from 'react'
 import { Route, Switch } from 'react-router-dom'
+import { ProgressContextProvider } from './store/progress_context'
 import Main from './pages/Main'
 import AboutMe from './pages/AboutMe'
 import Certifications from './pages/Certifications'
 import Skills from './pages/Skills'
 import Projects from './pages/Projects'
+import ContactMe from './pages/ContactMe'
 import Navbar from './components/UI/Navbar'
 import ProgressBar from './components/UI/ProgressBar'
 import styles from './App.module.css'
+import logo from './images/logo.png'
 
 function App() {
     return (
         <Fragment>
-            <img className={styles.logo} src='/logo.png' alt='logo'/>
-            <ProgressBar/>
-            <Navbar/>
+            <img className={styles.logo} src={logo} alt='logo'/>
+            <ProgressContextProvider>
+                <Navbar/>
+                <ProgressBar/>
+            </ProgressContextProvider>
             <Switch>
                 <Route path="/" exact>
                     <Main/>
@@ -30,6 +35,9 @@ function App() {
                 </Route>
                 <Route path="/projects">
                     <Projects/>
+                </Route>
+                <Route path="/contact-me">
+                    <ContactMe/>
                 </Route>
             </Switch>
         </Fragment>
---
+++ lpf/src/pages/ContactMe.js
@@ -0,0 +1,9 @@
+import React, {Fragment} from 'react'
+
+function ContactMe(){
+    return (
+        <Fragment></Fragment>
+    )
+}
+
+export default ContactMe
