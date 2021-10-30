e6334a73d4b4f42b262d5a2029e4be39a320b449
Luis Adolfo Rodriguez Lopez <adolfoluis.rl@gmail.com>
2021-10-29 19:30:00 -0600

    Add the about me page with it's respective routing

--- lpf/src/App.js
+++ lpf/src/App.js
@@ -1,9 +1,11 @@
-import React, {Fragment} from 'react'
+import React, { Fragment } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Main from './pages/Main'
+import AboutMe from './pages/AboutMe'
 import Skills from './pages/Skills'
 import Projects from './pages/Projects'
 import Navbar from './components/UI/Navbar'
+import styles from './App.module.css'
 
 function App() {
     return (
@@ -12,6 +14,9 @@ function App() {
             <Switch>
                 <Route path="/" exact>
                     <Main/>
+                </Route>
+                <Route path="/about-me">
+                    <AboutMe/>
                 </Route>
                 <Route path="/skills">
                     <Skills/>
