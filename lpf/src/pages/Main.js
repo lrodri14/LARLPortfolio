26fba2f76e491165cd3288060e49f2b86be0d531
Luis Adolfo Rodriguez Lopez <adolfoluis.rl@gmail.com>
2021-10-31 21:17:45 -0600

    Main Component, code refactor

--- lpf/src/pages/Main.js
+++ lpf/src/pages/Main.js
@@ -1,29 +1,16 @@
-import React, { Fragment, useContext } from 'react'
-import ProgressContext from '../store/progress_context'
-import { Link } from 'react-router-dom'
+import React, { Fragment } from 'react'
+import NextButton from '../components/UI/NextButton'
 import pp from '../images/pp-bw.png'
 import styles from './Main.module.css'
 
 
 function Main(){
-    // Context and States
-    const ctx = useContext(ProgressContext)
-
-    // Handlers
-    function aboutMeButtonClickHandler(){
-        const activeSection = {
-            'text': 'About Me',
-            'class': 'about-me'
-        }
-        ctx.activateSection(activeSection)
-    }
-
     return (
         <Fragment>
             <div className={styles.welcome}>
                 <h1 className={styles.welcome__message}>Hello, my name is Luis Adolfo <br/> I'm a Full Stack Developer</h1>
                 <p>Want to know more about me?</p>
-                <Link to='/about-me' className={styles['welcome__about-me-button']} onClick={aboutMeButtonClickHandler}><i className="fas fa-play"></i> About Me</Link>
+                <NextButton to='/about-me' activateClass={'about-me'}>About Me</NextButton>
             </div>
             <img className={styles['profile-pic']} src={pp} alt="Luis Rodriguez's Portfolio Pic" />
             <a href='https://www.linkedin.com/in/larl/' target="_blank" rel="noreferrer" className={styles.linkedIn}><i className="fab fa-linkedin"></i></a>
