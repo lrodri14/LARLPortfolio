24387bdb7fc48f0a598bd5e2e492e62d329285e4
Luis Adolfo Rodriguez Lopez <adolfoluis.rl@gmail.com>
2021-11-01 08:47:15 -0600

    Initial Setup for the Skills Page

--- lpf/src/pages/Skills.js
+++ lpf/src/pages/Skills.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import styles from './Skills.module.css'
+
+const categories = ['Speaking Languages', 'Tools and Technologies', 'Industry Knowledge']
+const skills = {
+    languages: ['Spanish', 'English'],
+    tools: [{toolType: 'Language', toolName: 'Python', expertise: 'Experienced'},
+            {toolType: 'Framework', toolName: 'Django', expertise: 'Experienced'},
+            {toolType: 'Language', toolName: 'Javascript', expertise: 'Advanced'},
+            {toolType: 'Library', toolName: 'React', expertise: 'Beginner'},
+            {toolType: 'Language', toolName: 'HTML/CSS', expertise: 'Advanced'},
+            {toolType: 'Library', toolName: 'D3.js', expertise: 'Beginner'},
+            ],
+    industryKnowledge: ['Web Development']
+}
 
 function Skills(){
-    return <h1>This is the skills page</h1>
+    return (
+        <div className={styles.skills}>
+            <div className={styles.skills__cards}></div>
+            <div className={styles.skills__graph}></div>
+        </div>
+    )
 }
 
 export default Skills
