3c20acf28ec15a909deb76362164182633b832c8
Luis Adolfo Rodriguez Lopez <adolfoluis.rl@gmail.com>
2021-12-27 18:10:08 -0600

    Mail content sending with request

--- lpf/src/components/ContactMe/ContactMeForm.js
+++ lpf/src/components/ContactMe/ContactMeForm.js
@@ -45,6 +45,8 @@ function ContactMeForm(){
     const [isLoading, setIsLoading] = useState(false)
     const [formIsValid, setFormIsValid] = useState(false)
     const [formState, formDispatch] = useReducer(formValidityReducer, initialFormState)
+    const [success, setSuccess] = useState('')
+    const [error, setError] = useState('')
 
     const fnRef = useRef()
     const lnRef = useRef()
@@ -56,6 +58,17 @@ function ContactMeForm(){
     useEffect(() => {
         setFormIsValid(firstNameIsValid && lastNameIsValid && emailIsValid && messageIsValid)
     }, [firstNameIsValid, lastNameIsValid, emailIsValid, messageIsValid])
+
+    async function sendMail(content){
+        const response = await fetch('http://127.0.0.1:8000/main/send_mail/',{method: 'POST',body: JSON.stringify(content),headers: { "Content-Type": "application/json", "Accept": "application/json" }})
+        setIsLoading(false)
+        if (!response.ok){
+            setError(response.error)
+            return
+        }
+        const data = await response.json()
+        setSuccess(data.data)
+    }
 
     function identificationInputHandler(event){
         const target = event.target
@@ -86,6 +99,10 @@ function ContactMeForm(){
         event.preventDefault()
         if (formIsValid){
             setIsLoading(true)
+            setSuccess('')
+            setError('')
+            const content = {first_name: formState.firstNameValue, last_name: formState.lastNameValue, email: formState.emailValue, message: formState.messageValue}
+            sendMail(content)
         }else if (!formState.firstNameIsValid){
             fnRef.current.focus()
         }else if (!formState.lastNameIsValid){
@@ -98,7 +115,7 @@ function ContactMeForm(){
     }
 
     return (
-        <form method="POST" autoComplete="off" className={styles['contact-me-form']} onSubmit={submitHandler}>
+        <form method="POST" autoComplete="on" className={styles['contact-me-form']} onSubmit={submitHandler}>
             {isLoading && <LoadingSpinner/>}
             <h2 className={styles['contact-me-form__title']}>Contact Me</h2>
             <label htmlFor="name">First Name</label>
@@ -125,11 +142,13 @@ function ContactMeForm(){
             <label htmlFor="message">Message</label>
             <textarea id="message"
                       cols="60"
-                      rows="10"
+                      rows="8"
                       className={formState.messageIsValid === false ? styles['contact-me-form__invalid-input'] : " "}
                       ref={messageRef}
                       onChange={messageInputHandler}
                       onBlur={touchedHandler}/>
+            {error && <p className={styles['contact-me-form-error']}>{error}</p>}
+            {success && <p className={styles['contact-me-form-success']}>{success}</p>}
             <button type="submit">Contact Me</button>
         </form>
     )
