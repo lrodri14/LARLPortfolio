8292dc3734af7945f3c645191e24631f7936ec66
Luis Adolfo Rodriguez Lopez <adolfoluis.rl@gmail.com>
2022-01-01 12:40:26 -0600

    Implement skills requesting and displaying functionality

--- lpf/src/components/UI/Card/GraphCard.js
+++ lpf/src/components/UI/Card/GraphCard.js
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import useHttp from '../../../hooks/useHttp'
 import Card from  './Card'
 import styles from './GraphCard.module.css'
 
 function GraphCard(props){
 
+    const { url } = props
     const [expanded, setExpanded] = useState(false)
+    const [skills, setSkills] = useState([])
+    const { isLoading, error, success, sendRequest } = useHttp()
+
+    function cleanSkills(data){
+        setSkills(data)
+    }
+
+    useEffect(() => {
+        sendRequest({endPoint: url}, cleanSkills)
+    }, [sendRequest, url])
 
     function clickHandler(){
         setExpanded((prevState) => {
@@ -14,8 +26,8 @@ function GraphCard(props){
 
     let content = <p>There are no skills to display at the moment</p>
 
-    if (props.skills){
-        content = props.skills.map((skill) => {
+    if (skills.length > 0){
+        content = skills.map((skill) => {
                     return (
                         <div className={styles['graph']} key={skill.name}>
                             <p>{skill.name} <i className={skill.icon}></i></p>
@@ -23,7 +35,7 @@ function GraphCard(props){
                                 <p className={`${styles['graph__labels__label']} ${styles['minimun']}`}>Novice</p>
                                 <p className={`${styles['graph__labels__label']} ${styles['maximum']}`}>Expert</p>
                             </div>
-                            <div className={styles['graph__bar']} style={{width: `${skill.expertise}%`}}></div>
+                            <div className={styles['graph__bar']} style={{width: `${skill.expertise_level}%`}}></div>
                         </div>
                     )
                 })
@@ -32,8 +44,10 @@ function GraphCard(props){
     return (
         <Card onClick={clickHandler} className={!expanded ? styles['graph-card'] : props.className}>
             <h2>{props.category}</h2>
-            {expanded && content}
-            <p>Click me to visualize/hide detailed information</p>
+            {expanded && !isLoading && !error && success && content}
+            {error && expanded &&  <p>{error}</p>}
+            {isLoading && expanded && <p>{`Loading ${props.category}...`}</p>}
+            {!expanded && <p>Click me to visualize/hide detailed information</p>}
         </Card>
     )
 }
