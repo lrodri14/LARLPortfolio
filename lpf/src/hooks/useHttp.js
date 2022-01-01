bf195a1eef97663f66810555c6fe2adbe1f5ed96
Luis Adolfo Rodriguez Lopez <adolfoluis.rl@gmail.com>
2022-01-01 15:56:34 -0600

    Fix bug related to error displaying in contact me form and error setting from returned response from request

--- lpf/src/hooks/useHttp.js
+++ lpf/src/hooks/useHttp.js
@@ -3,31 +3,37 @@ const url = 'http://127.0.0.1:8000/main/'
 
 function useHttp(){
     const [isLoading, setIsLoading] = useState(false)
-    const [success, setSuccess] = useState(false)
     const [error, setError] = useState(false)
-    const sendRequest = useCallback(async (requestConfig, cleanUpProcess) => {
+    const [response, setResponse] = useState(false)
+    const sendRequest = useCallback(async (requestConfig, cleanUpProcess = () => {}) => {
         try{
             setIsLoading(true)
-            setSuccess(false)
-            setError(false)
+                        setError(false)
+            setResponse(false)
             const response = await fetch(url + requestConfig.endPoint,
                                         {method: requestConfig.method ? requestConfig.method : 'GET',
                                         headers: requestConfig.headers ? requestConfig.headers : {},
                                         body: requestConfig.content ? JSON.stringify(requestConfig.content) : null })
 
             const data = await response.json()
-            cleanUpProcess(data)
+
+            if (!response.ok){
+                const error = new Error()
+                error.error = data.error
+                throw error
+            }
+
+            setResponse(data)
         }catch(error){
-            setError(error.message)
+            setError(error)
         }
         setIsLoading(false)
-        setSuccess(true)
     }, [])
 
     return {
         isLoading,
-        success,
         error,
+        response,
         sendRequest
     }
 }
