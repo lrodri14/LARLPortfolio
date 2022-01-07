0dcef33eb9f1d472f09b8b63341ebe4cf783607f
Luis Adolfo Rodriguez Lopez <adolfoluis.rl@gmail.com>
2022-01-06 18:53:13 -0600

    Setup projects request for projects response displaying

--- lpf/src/components/UI/Card/ProjectCard.js
+++ lpf/src/components/UI/Card/ProjectCard.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
 import Card from './Card'
-import sealenaLogo from '../../../images/sealena-logo.png'
 import styles from './ProjectCard.module.css'
 
 function ProjectCard(props){
 
     const [flip, setFlip] = useState(false)
+    const { project } = props
 
     function projectClickHandler(){
         setFlip((prevState) => {
@@ -17,18 +17,18 @@ function ProjectCard(props){
             <Card className={`styles['project-card'] ${flip && styles['project-card__rotate']}`} onClick={projectClickHandler}>
                 {!flip &&
                     <div className={styles['project-card__details']}>
-                        <img src={sealenaLogo} alt="" className={styles['project-card__logo']} />
-                        <h2>{props.name}</h2>
-                        <p><i className="fas fa-hourglass-start"></i> {props.startDate}</p>
-                        <p><i className="fas fa-hourglass-end"></i> {props.endDate}</p>
-                        <p>Go to: <a href={props.url} target="_blank" rel="noreferrer">{props.url}</a></p>
+                        <img src={project.logo} alt="" className={styles['project-card__logo']} />
+                        <h2>{project.name}</h2>
+                        <p><i className="fas fa-hourglass-start"></i> {project.start_date}</p>
+                        <p><i className="fas fa-hourglass-end"></i> {project.end_date}</p>
+                        <p>Go to: <a href={props.url} target="_blank" rel="noreferrer">{project.url}</a></p>
                     </div>
                 }
                 {flip &&
                     <div className={styles['project-card__description']}>
-                        <img src={sealenaLogo} alt="" className={styles['project-card__logo']} />
-                        <h2>{props.name}</h2>
-                        <p>{props.description}</p>
+                        <img src={project.logo} alt="" className={styles['project-card__logo']} />
+                        <h2>{project.name}</h2>
+                        <p>{project.description}</p>
                     </div>
                 }
             </Card>
--- lpf/src/pages/Projects.js
+++ lpf/src/pages/Projects.js
@@ -1,27 +1,24 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
+import useHttp from '../hooks/useHttp'
 import Board from '../components/UI/Board/Board'
 import NextButton from '../components/UI/NextButton/NextButton'
 import ProjectCard from '../components/UI/Card/ProjectCard'
 import styles from './Projects.module.css'
 
-
-const projects = [
-    {
-         name: 'Sealena',
-         startDate:'June 6, 2020',
-         endDate:'September 4, 2021',
-         url:'https://www.sealena.com',
-         description:"Sealena it's a Medical Information Management and Collaboration tool that helps Health Workers organize and communicate with their patients and colleagues easily."
-    },
-]
-
 function Projects(){
+    const {response: projects, sendRequest} = useHttp()
+
+    useEffect(() => {
+        sendRequest({endPoint: 'projects/'})
+    }, [sendRequest])
+
+
     return (
         <Fragment>
             <Board>
-                {projects.map((project) => {
+                {projects && projects.map((project) => {
                     return (
-                        <ProjectCard key={project.name} name={project.name} startDate={project.startDate} endDate={project.endDate} url={project.url} description={project.description}/>
+                        <ProjectCard key={project.name} project={project}/>
                     )
                 })}
             </Board>
